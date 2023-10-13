f010d3c9ca8a337db07adab228f1edfa28bab968
Bucklan <as1kenjebaev@gmail.com>
2023-10-13 18:57:41 +0600

    Add user and post controllers
    Moved user related logic (registration, login, getMe) to a separate UserController.js. This was done to maintain a consistent approach to function organization, which should make code maintenance easier.
    
    Also started to introduce PostController.js for handling post related requests. The file at this point is empty, however in future commits, handling for creating posts will be added.

--- index.js
+++ index.js
@@ -6,119 +6,16 @@ import registerValidation from "./validations/auth.js";
 import {validationResult} from "express-validator";
 import UserModule from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
+import * as UserController from "./controllers/UserController.js";
 
 const app = express();
 app.use(express.json());
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.4kuzydd.mongodb.net/blog')
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err))
-app.post('/login', async (req, res) => {
-    try {
-        const user = await UserModule.findOne({email: req.body.email});
-
-        if (!user) {
-            return res.status(404).json({
-                message: 'Users not found'
-            });
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.password);
-
-        if (!isValidPass) {
-            return res.status(404).json({
-                message: 'incorrect email or password',
-            });
-        }
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d'
-            }
-        );
-        const {password, ...userData} = user._doc
-
-        res.json({
-            ...userData,
-            token
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: "login to failed"
-        });
-    }
-});
-app.post('/register', registerValidation, async (req, res) => {
-
-    try {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array());
-        }
-
-        const requestPassword = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(requestPassword, salt);
-
-        const doc = new UserModule({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            password: hash,
-        });
-
-        const user = await doc.save();
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d'
-            }
-        );
-        const {password, ...userData} = user._doc
-
-        res.json({
-            ...userData,
-            token
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: "register to failed"
-        });
-    }
-
-});
-app.get('/auth/me', checkAuth, async (req, res) => {
-    try {
-        const user = await UserModule.findById(req.userId);
-
-
-        if (!user) {
-            return res.status(404).json({
-                message: "users not found"
-            });
-        }
-
-
-        const {password, ...userData} = user._doc
-
-        res.json(userData);
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: "register to failed"
-        });
-    }
-})
+app.post('/login', UserController.login);
+app.post('/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe)
 app.listen(4444, (err) => {
     if (err) {
         return console.log(err)
